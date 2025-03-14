d68be0af565f6c094fb30f9ebb2b3d8181b686cd
Jinhyeok <jinhyuk@unist.ac.kr>
2025-03-14 19:57:09 +0900

    ✨ DFA scene manager

--- examples/datasets/diva360.py
+++ examples/datasets/diva360.py
@@ -5,20 +5,20 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def c2w_to_colmap(c2w):
-    c2w[2,:] *= -1 # flip whole world upside down
-    c2w = c2w[[1,0,2,3],:]
-    c2w[0:3,1] *= -1 # flip the y and z axis
-    c2w[0:3,2] *= -1
-    
+def diva360_to_colmap(c2w):
+    c2w[2, :] *= -1  # flip whole world upside down
+    c2w = c2w[[1, 0, 2, 3], :]
+    c2w[0:3, 1] *= -1  # flip the y and z axis
+    c2w[0:3, 2] *= -1
+
     w2c = np.linalg.inv(c2w)
     rot = w2c[:3, :3]
     tvec = w2c[:3, -1]
-    
+
     rotation = R.from_matrix(rot)
     qvec = rotation.as_quat()  # Returns [x, y, z, w]
     qvec = np.array(qvec)[[3, 0, 1, 2]]
-    
+
     return qvec, tvec
 
 
@@ -60,7 +60,7 @@ class SceneManagerDiva360:
             transform = np.array(frame.get("transform_matrix", []))
 
             if transform.shape == (4, 4):
-                qvec, tvec = c2w_to_colmap(transform)
+                qvec, tvec = diva360_to_colmap(transform)
                 q = Quaternion(qvec)
 
                 # Create an Image object
@@ -70,7 +70,6 @@ class SceneManagerDiva360:
                 self.name_to_image_id[file_name] = camera_id
 
                 self.last_image_id = max(self.last_image_id, camera_id)
-            
 
     def genrate_random_points3D(self, n=100000):
         translation_list = np.empty((len(self.images), 3))
