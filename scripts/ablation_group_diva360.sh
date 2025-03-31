8c5a5fa895718a9834b9c18058ac5b9cf59d1f3d
Jinhyeok <jinhyuk@unist.ac.kr>
2025-03-31 14:42:40 +0900

    .

--- scripts/ablation_group_diva360.sh
+++ scripts/ablation_group_diva360.sh
@@ -9,22 +9,28 @@ for i in {0..0}
 do
     scripts_name=${scripts_name_list[$i]}
     wandb_group_name=${wandb_group_name_list[$i]}
-    bash scripts/$scripts_name 0 wolf 0000 2393 $wandb_group_name &
-    bash scripts/$scripts_name 0 horse 0120 0375 $wandb_group_name &
+    bash scripts/$scripts_name 0 wolf 0000 2393 $wandb_group_name &&
+    bash scripts/$scripts_name 0 horse 0120 0375 $wandb_group_name &&
     bash scripts/$scripts_name 0 k1_push_up 0370 0398 $wandb_group_name &
-    bash scripts/$scripts_name 2 k1_hand_stand 0000 0300 $wandb_group_name &
-    bash scripts/$scripts_name 2 music_box 0100 0125 $wandb_group_name &
+
+    bash scripts/$scripts_name 2 k1_hand_stand 0000 0300 $wandb_group_name &&
+    bash scripts/$scripts_name 2 music_box 0100 0125 $wandb_group_name &&
     bash scripts/$scripts_name 2 trex 0100 0300 $wandb_group_name &
-    bash scripts/$scripts_name 3 k1_double_punch 0000 0555 $wandb_group_name &
+
+    bash scripts/$scripts_name 3 k1_double_punch 0000 0555 $wandb_group_name &&
     bash scripts/$scripts_name 3 world_globe 0020 0074 $wandb_group_name &
-    bash scripts/$scripts_name 4 blue_car 0142 0214 $wandb_group_name &
+
+    bash scripts/$scripts_name 4 blue_car 0142 0214 $wandb_group_name &&
     bash scripts/$scripts_name 4 truck 0078 0171 $wandb_group_name &
-    bash scripts/$scripts_name 5 clock 0000 1500 $wandb_group_name &
+
+    bash scripts/$scripts_name 5 clock 0000 1500 $wandb_group_name &&
     bash scripts/$scripts_name 5 penguin 0217 0239 $wandb_group_name &
-    bash scripts/$scripts_name 6 wall_e 0222 0286 $wandb_group_name &
+
+    bash scripts/$scripts_name 6 wall_e 0222 0286 $wandb_group_name &&
     bash scripts/$scripts_name 6 bunny 0000 1000 $wandb_group_name &
-    bash scripts/$scripts_name 7 red_car 0042 0250 $wandb_group_name &
-    bash scripts/$scripts_name 7 dog 0177 0279 $wandb_group_name &
+
+    bash scripts/$scripts_name 7 red_car 0042 0250 $wandb_group_name &&
+    bash scripts/$scripts_name 7 dog 0177 0279 $wandb_group_name &&
     bash scripts/$scripts_name 7 stirling 0000 0045 $wandb_group_name &
 
     wait
@@ -32,5 +38,4 @@ do
     trap "exit" INT
 
 done
-
 
