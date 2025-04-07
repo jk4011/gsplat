a83df407d8e52e4e928488c0948f412f761b5dd7
Jinhyeok <jinhyuk@unist.ac.kr>
2025-04-07 16:23:13 +0900

    .

--- scripts/link_from_file_server.sh
+++ scripts/link_from_file_server.sh
@@ -4,9 +4,13 @@ set -e        # exit when error
 set -o xtrace # print command
 
 # mkdir data if not exist
-mkdir -p /data2/wlsgur4011/GESI/gsplat/data
+data_dir=/data2/wlsgur4011/GESI/gsplat/data
+mkdir -p $data_dir
 
-ln -s /data/wlsgur4011/GESI/DFA_processed /data2/wlsgur4011/GESI/gsplat/data
-ln -s /data/wlsgur4011/GESI/diva360_processed /data2/wlsgur4011/GESI/gsplat/data
+ln -s /data/wlsgur4011/GESI/DFA_processed $data_dir
+ln -s /data/rvi/dataset/Diva360_data $data_dir
+ln -s /data/wlsgur4011/DFA $data_dir
+ln -s /data/wlsgur4011/GESI/diva360_processed $data_dir
 ln -s /data/wlsgur4011/GESI/results /data2/wlsgur4011/GESI/gsplat/results
+
 ln -s /data/wlsgur4011/DFA/.cache /tmp/
