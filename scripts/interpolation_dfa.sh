eadc1312b2414f3fde33df8acac25aad5961f8fe
Jinhyeok <jinhyuk@unist.ac.kr>
2025-04-07 14:24:17 +0900

    ✨ interpolation dfa

--- scripts/interpolation_dfa.sh
+++ scripts/interpolation_dfa.sh
@@ -4,44 +4,51 @@ set -e        # exit when error
 # set -o xtrace # print command
 
 GPU=$1
-object_name=$2
+object_name="$2"
 
 declare -A idx_dict
-idx_dict["wall_e"]="0222 0286 0303 0377 0387"
-idx_dict["k1_double_punch"]="0331 0339 0344 0346 0355"
-idx_dict["penguin"]="0047 0067 0160 0180 0654"
+idx_dict["fox(attitude)"]="90 110 125 150 170"
+idx_dict["whiteTiger(run)"]="5 10 15 20 25"
+idx_dict["whiteTiger(roaringwalk)"]="0 5 10 15 20"
+idx_dict["wolf(Damage)"]="0 50 110 135 170"
+idx_dict["wolf(Howling)"]="5 30 75 145 170"
+idx_dict["cat(walkprogressive_noz)"]="15 20 25 30 35"
+idx_dict["duck(eat_grass)"]="0 5 10 15 20"
+idx_dict["wolf(Walk)"]="70 80"
 
 declare -A cam_dict
-cam_dict["wall_e"]="0"
-cam_dict["k1_double_punch"]="1"
-cam_dict["penguin"]="0"
+cam_dict["fox(attitude)"]="32"
+cam_dict["whiteTiger(run)"]="32"
+cam_dict["whiteTiger(roaringwalk)"]="32"
+cam_dict["wolf(Damage)"]="32"
+cam_dict["wolf(Howling)"]="32"
+cam_dict["cat(walkprogressive_noz)"]="32"
+cam_dict["duck(eat_grass)"]="32"
+cam_dict["wolf(Walk)"]="32"
 
 cam_idx=${cam_dict[$object_name]}
 idx_list=(${idx_dict[$object_name]})
 
-for idx in ${idx_list[@]:0}; do
-    bash scripts/preprocess_diva360.sh $object_name $idx | true
-done
-
 idx_from=${idx_list[0]}
 idx_to=${idx_list[1]}
 
-ckpt=./results/diva360/${object_name}_${idx_from}/ckpts/ckpt_best_psnr.pt
+ckpt=./results/dfa/${object_name}_${idx_from}/ckpts/ckpt_best_psnr.pt
+
 idx_start=${idx_list[0]}
 
-if [ ! -f $ckpt ]; then
-    bash scripts/train_diva360.sh $GPU $object_name $idx_from
+if [ ! -f "$ckpt" ]; then
+    bash scripts/train_dfa.sh $GPU $object_name $idx_from
 fi
 
 for idx_to in ${idx_list[@]:1}; do
-    result_dir=./results/diva360_finetune/${object_name}_${idx_start}_${idx_from}_${idx_to}
+    result_dir=./results/dfa_finetune/${object_name}_${idx_start}_${idx_from}_${idx_to}
     if [ ! -f $result_dir/ckpt_finetune.pt ]; then
         CUDA_VISIBLE_DEVICES=$GPU python examples/simple_trainer.py default \
-            --data_dir /data2/wlsgur4011/GESI/gsplat/data/diva360_processed/${object_name}_${idx_to}/ \
+            --data_dir /data2/wlsgur4011/GESI/gsplat/data/DFA_processed/${object_name}/${idx_to}/ \
             --result_dir $result_dir \
             --ckpt $ckpt \
             --data_factor 1 \
-            --data_name diva360 \
+            --data_name DFA \
             --single_finetune \
             --port 8081 \
             --scale_reg 0.1 \
@@ -61,14 +68,14 @@ echo make motion video
 idx_from=${idx_list[0]}
 idx_to=${idx_list[1]}
 for idx_to in ${idx_list[@]:1}; do
-    result_dir=./results/diva360_finetune/${object_name}_${idx_start}_${idx_from}_${idx_to}
+    result_dir=./results/dfa_finetune/${object_name}_${idx_start}_${idx_from}_${idx_to}
 
     CUDA_VISIBLE_DEVICES=$GPU python examples/simple_trainer.py default \
-        --data_dir /data2/wlsgur4011/GESI/gsplat/data/diva360_processed/${object_name}_${idx_to}/ \
+        --data_dir /data2/wlsgur4011/GESI/gsplat/data/DFA_processed/${object_name}/${idx_to}/ \
         --result_dir $result_dir \
         --ckpt $ckpt \
         --data_factor 1 \
-        --data_name diva360 \
+        --data_name DFA \
         --single_finetune \
         --port 8081 \
         --scale_reg 0.1 \
@@ -87,18 +94,18 @@ done
 idx_from=${idx_list[0]}
 idx_to=${idx_list[1]}
 for idx_to in ${idx_list[@]:1}; do
-    video_path=/data2/wlsgur4011/GESI/output_video/diva360_simple/${object_name}_${idx_start}_${idx_from}_${idx_to}_0.mp4
+    video_path=/data2/wlsgur4011/GESI/output_video/DFA_simple/${object_name}_${idx_start}_${idx_from}_${idx_to}_0.mp4
     video_paths="$video_paths ${video_path}"
     idx_from=$idx_to
 done
 
 for idx in ${idx_list[@]:0}; do
-    image_path=/data2/wlsgur4011/GESI/gsplat/data/diva360_processed/${object_name}_${idx}/images/cam0${cam_idx}.png
+    image_path=/data2/wlsgur4011/GESI/gsplat/data/DFA_processed/${object_name}/${idx}/images/img_00${cam_idx}_rgba.png
     image_paths="$image_paths ${image_path}"
 done
 
-mkdir -p /data2/wlsgur4011/GESI/output_video_interpolated/diva360/ | true
+mkdir -p /data2/wlsgur4011/GESI/output_video_interpolated/dfa/ | true
 python ../gesi/video_integration.py \
     --image_paths $image_paths \
     --video_paths $video_paths \
-    --output_path /data2/wlsgur4011/GESI/output_video_interpolated/diva360/${object_name}.mp4
+    --output_path /data2/wlsgur4011/GESI/output_video_interpolated/dfa/${object_name}.mp4
